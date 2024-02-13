c1d5b225f8a83c3962786da096980d8fa4eaa8c5
Dev Femi Badmus <devfemibadmus@gmail.com>
2024-02-13 20:11:55 +0100

    domains and its user list only

--- server/admin.py
+++ server/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import CustomUser
+from .models import CustomUser, Domain
 
 admin.site.register(CustomUser)
+admin.site.register(Domain)
