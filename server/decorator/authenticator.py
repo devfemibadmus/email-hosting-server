d57d9a85b62b32131a79032b7ce17722b6a81363
Dev Femi Badmus <devfemibadmus@gmail.com>
2024-02-13 15:52:54 +0100

    domain auth decorator

--- server/decorator/authenticator.py
+++ server/decorator/authenticator.py
@@ -8,15 +8,17 @@ def resolve_domain_record(view_func):
         domain = kwargs.get('domain') or request.GET.get('domain') or request.POST.get('domain')
         if domain:
             checker = DNSChecker()
-            request.mx_record = checker.verify_mx(domain, "mail.blackstackhub.com.")
+            request.mx_record = checker.verify_mx(domain)
             request.txt_record = checker.verify_txt(domain, request.user.txt_record)
-            if mx_record:
-                if mx_record != "mail.blackstackhub.com":
-                    messages.error(request, f"MX record pointing to {mx_record}")
+            if request.mx_record:
+                if request.mx_record != "mail.blackstackhub.com.":
+                    messages.error(request, f"MX record pointing to {request.mx_record}")
                 else:
-                    messages.success(request, f"MX record pointing to {mx_record}")
+                    messages.success(request, f"MX record pointing to {request.mx_record}")
             else:
                 messages.error(request, f"MX record not found for the {domain}")
+            if not request.txt_record:
+                messages.error(request, f"TXT record not found for the {domain}")
         else:
             messages.error(request, "Domain parameter is missing")
         
