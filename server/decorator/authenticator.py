de42e81079b5396385d32614d9eb9ba6712736b5
Dev Femi Badmus <devfemibadmus@gmail.com>
2024-02-13 15:12:03 +0100

    using DNSChecker now

--- server/decorator/authenticator.py
+++ server/decorator/authenticator.py
@@ -1,19 +1,16 @@
 from functools import wraps
 from django.contrib import messages
-from ..operator.dns_resolver import DomainResolver
+from ..operator.dns_checker import DNSChecker
 
 def resolve_domain_record(view_func):
     @wraps(view_func)
     def wrapper(request, *args, **kwargs):
         domain = kwargs.get('domain') or request.GET.get('domain') or request.POST.get('domain')
         if domain:
-            resolver = DomainResolver()
-            resolver.QUERY_TYPE = resolver.QUERY_TYPE_MX
-            mx_record = resolver.resolve_domain(domain)
-            resolver.QUERY_TYPE = resolver.QUERY_TYPE_TXT
-            txt_record = resolver.resolve_domain(domain)
+            checker = DNSChecker()
+            request.mx_record = checker.verify_mx(domain, "mail.blackstackhub.com.")
+            request.txt_record = checker.verify_txt(domain, request.user.txt_record)
             if mx_record:
-                request.mx_record = mx_record
                 if mx_record != "mail.blackstackhub.com":
                     messages.error(request, f"MX record pointing to {mx_record}")
                 else:
