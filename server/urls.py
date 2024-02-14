e5b09cbd47f06f2fd71fc506b0ceb0392482feb0
Dev Femi Badmus <devfemibadmus@gmail.com>
2024-02-14 10:00:00 +0100

    messaging url path domain

--- server/urls.py
+++ server/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.server, name='server'),
-    path('domain/', views.DomainView.as_view(), name='domain_with_param'),
-    path('domain/<str:domain>/', views.DomainView.as_view(), name='domain_with_param'),
+    path('message/', views.MessagesView.as_view(), name='domain'),
+    path('domain/', views.DomainView.as_view(), name='domain'),
+    path('domain/<str:domain>/', views.DomainView.as_view(), name='domain'),
 ]
