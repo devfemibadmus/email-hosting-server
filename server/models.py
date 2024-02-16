4ac33d0140b8c9f4030392547fdfdbf2c4a4c383
Dev Femi Badmus <devfemibadmus@gmail.com>
2024-02-16 23:19:21 +0100

    getting ready, update model shift test

--- server/models.py
+++ server/models.py
@@ -3,8 +3,17 @@ from django.conf import settings
 from django.db import models
 
 
+
+class EmailSettings(models.Model):
+    smtp_username = models.CharField(max_length=255, blank=True, null=True)
+    smtp_password = models.CharField(max_length=255, blank=True, null=True)
+    use_tls = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Email Settings for {self.user.email}"
 class CustomUser(AbstractUser):
     txt_record = models.CharField(max_length=255, default='txt')
+    email_settings = models.OneToOneField("EmailSettings",on_delete=models.CASCADE,null=True,blank=True,related_name="user",)
 
     def save(self, *args, **kwargs):
         if not self.txt_record.startswith(f"{settings.MX_RECORD}"):
@@ -17,8 +26,6 @@ class CustomUser(AbstractUser):
     def get_owned_domains(self):
         return Domain.objects.filter(user=self)
 
-
-
 class Domain(models.Model):
     name = models.CharField(max_length=100)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
@@ -27,6 +34,4 @@ class Domain(models.Model):
     def __str__(self):
         return self.name
 
-    def get_emails(self):
-        return EmailMessage.objects.filter(domain=self)
 
