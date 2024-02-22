e3d655456e82cae01ffe205dc776be867be6bd33
Dev Femi Badmus <devfemibadmus@gmail.com>
2024-02-22 11:00:00 +0100

    url for now

--- server/urls.py
+++ server/urls.py
@@ -3,7 +3,13 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.server, name='server'),
-    path('message/', views.MessagesView.as_view(), name='domain'),
+
     path('domain/', views.DomainView.as_view(), name='domain'),
-    path('domain/<str:domain>/', views.DomainView.as_view(), name='domain'),
+    path('<str:domain>/', views.DomainView.as_view(), name='domain_name'),
+    path('domain/<str:domain>/', views.DomainView.as_view(), name='domain__domain_name'),
+    
+
+    path('domain/mails/', views.MessagesView.as_view(), name='mails'),
+    path('<str:domain>/mails/', views.MessagesView.as_view(), name='domains_name__mails'),
+    path('domain/<str:domain>/mails/', views.MessagesView.as_view(), name='domain__domain_name__mails'),
 ]
