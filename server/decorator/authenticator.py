bec7ace87ad5a7b8b943d342fd1c4822d7ae46da
Dev Femi Badmus <devfemibadmus@gmail.com>
2024-02-23 10:00:00 +0100

    new model name VirtualDomain

--- server/decorator/authenticator.py
+++ server/decorator/authenticator.py
@@ -2,33 +2,36 @@ from functools import wraps
 from django.conf import settings
 from django.contrib import messages
 from ..operator.dns_checker import DNSChecker
-from ..models import Domain
+from ..models import VirtualDomain
 
 def resolve_domain_record(view_func):
     @wraps(view_func)
     def wrapper(request, *args, **kwargs):
-        domain = kwargs.get('domain') or request.GET.get('domain') or request.POST.get('domain')
-        if domain:
+        domain_name = kwargs.get('domain') or request.GET.get('domain') or request.POST.get('domain')
+        if domain_name:
             checker = DNSChecker()
-            request.mx_record = checker.verify_mx(domain, settings.MX_RECORD)
-            request.txt_record = checker.verify_txt(domain, request.user.txt_record)
-            if request.mx_record == True:
-                messages.error(request, "MX record found")
-            elif request.mx_record == False:
+            mx_record = checker.verify_mx(domain_name, settings.MX_RECORD)
+            txt_record = checker.verify_txt(domain_name, request.user.txt_record)
+            
+            if mx_record is True:
+                messages.success(request, "MX record found")
+            elif mx_record is False:
                 messages.error(request, "MX record not found")
             else:
-                messages.error(request, request.mx_record)
-            if request.txt_record == True:
-                messages.success(request, "my TXT record found")
-                domain_obj, available = Domain.objects.get_or_create(user=request.user, name=domain, txt_record=request.user.txt_record)
+                messages.error(request, mx_record)
+            
+            if txt_record is True:
+                messages.success(request, "TXT record found")
+                domain_obj, created = VirtualDomain.objects.get_or_create(name=domain_name, user=request.user, txt_record=request.user.txt_record)
                 request.domain_obj = domain_obj
-            elif request.txt_record == False:
-                messages.success(request, "my TXT record not found")
+            elif txt_record is False:
+                messages.error(request, "TXT record not found")
+                messages.info(request, f'Add txt record {request.user.txt_record}')
             else:
-                messages.error(request, request.txt_record)
+                messages.error(request, txt_record)
         else:
             messages.error(request, "Domain parameter is missing")
-        
+
         return view_func(request, *args, **kwargs)
     
     return wrapper
