1a81189608833242d55986f3eaaf0caf6031e289
Dev Femi Badmus <devfemibadmus@gmail.com>
2024-02-24 11:00:00 +0100

    new model name VirtualDomain

--- server/views.py
+++ server/views.py
@@ -1,14 +1,12 @@
 from .decorator.mail import get_mail, create_mail, delete_mail
 from .decorator.authenticator import resolve_domain_record
 from django.contrib.auth.decorators import login_required
-from .models import EmailMessage, CustomUser, Domain
 from django.utils.decorators import method_decorator
 from django.shortcuts import render, redirect
 from django.core.serializers import serialize
 from django.http import JsonResponse
 from django.contrib import messages
 from django.views import View
-from .models import Domain
 
 
 @login_required
@@ -55,23 +53,12 @@ class MessagesView(View):
     @method_decorator(delete_mail)
     def dispatch(self, request, *args, **kwargs):
         email_message = kwargs.get('email_message')
-        email_message_dict = {
-            'sender': email_message.sender,
-            'sendto': email_message.sendto,
-            'domain': email_message.domain.name,
-            'subject': email_message.subject,
-            'body': email_message.body,
-            'sent_at': email_message.sent_at.isoformat() if email_message.sent_at else None,
-            'is_read': email_message.is_read,
-            'is_starred': email_message.is_starred,
-            'security_info': None
-        }
         messages_list = {message.tags: message.message for message in messages.get_messages(request)}
         response_data = {
-            'email_message': email_message_dict,
+            'email_message': email_message,
             'messages': messages_list
         }
-        if not request.GET.get("integrate"):
-            return JsonResponse(response_data)
-        return render(request, 'server/email.html', {'email_message': email_message, 'messages': messages_list})
+        return JsonResponse(response_data)
+
+
 
