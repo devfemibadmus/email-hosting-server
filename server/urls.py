e4d85340f69e6bcf012adc60b124eb56e7c3056c
Dev Femi Badmus <devfemibadmus@gmail.com>
2024-02-28 10:00:00 +0100

    remove alt going straigth to point

--- server/urls.py
+++ server/urls.py
@@ -2,11 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.server, name='server'),
-
-    path('domain/', views.DomainView.as_view(), name='domain'),
-    path('<str:domain>/', views.DomainView.as_view(), name='domain_name'),
-    path('domain/<str:domain>/', views.DomainView.as_view(), name='domain__domain_name'),
+    path('', views.DomainView.as_view(), name='domain'),
     
 
     path('domain/mails/', views.MessagesView.as_view(), name='mails'),
