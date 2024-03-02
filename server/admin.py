1fdaadd7aaefc4cb4cf91e2bcb28869676b67ffa
Dev Femi Badmus <devfemibadmus@gmail.com>
2024-03-02 02:16:53 +0100

    VirtualDomains

--- server/admin.py
+++ server/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import CustomUser, VirtualDomain
+from .models import CustomUser, VirtualDomains
 
 admin.site.register(CustomUser)
-admin.site.register(VirtualDomain)
+admin.site.register(VirtualDomains)
