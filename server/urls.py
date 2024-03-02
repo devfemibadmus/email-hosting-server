867c14359e47334fbd31751c08cb6c84d9fbee58
Dev Femi Badmus <devfemibadmus@gmail.com>
2024-03-02 13:42:34 +0100

    view domain

--- server/urls.py
+++ server/urls.py
@@ -2,10 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.DomainView.as_view(), name='domain'),
-    
-
-    path('domain/mails/', views.MessagesView.as_view(), name='mails'),
-    path('<str:domain>/mails/', views.MessagesView.as_view(), name='domains_name__mails'),
-    path('domain/<str:domain>/mails/', views.MessagesView.as_view(), name='domain__domain_name__mails'),
+    path('', views.DomainView.as_view(), name='server'),
+    path('<str:domain>/', views.DomainView.as_view(), name='domain'),
+    path('mail/<str:domain>/', views.MessagesView.as_view(), name='domain_mails'),
 ]
